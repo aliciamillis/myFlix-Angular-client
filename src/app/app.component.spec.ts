55e923e8cff503aaec8e498794541e3dedc0f8e7
aliciamillis <69804543+aliciamillis@users.noreply.github.com>
2021-03-19 13:46:24 -0600

    6.3 components and templates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myFlix-Angular-client'`, () => {
+  it(`should have as title 'flixNET-Angular-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myFlix-Angular-client');
+    expect(app.title).toEqual('flixNET-Angular-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('myFlix-Angular-client app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('flixNET-Angular-client app is running!');
   });
 });
